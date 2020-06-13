13981209308e5b4b816bb953d2ce79820b09be05
Julian Amirian <jamirian0714@gmail.com>
2020-06-13 13:58:40 -0700

    Initialize repo

---
+++ api/app.py
@@ -0,0 +1,15 @@
+import time
+from flask import Flask
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route('/')
+def index():
+	return app.send_static_file('index.html')
+
+@app.route('/api/time')
+def getCurrentTime():
+	return {'time': time.time()}
+
+if __name__ == '__main__':
+	app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      current_time: 0
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/time').then(res => res.json()).then(data => this.setState({
+      current_time: data.time
+    }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>The current time is {this.state.current_time}</p>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
